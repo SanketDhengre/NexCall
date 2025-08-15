e137c44981bc998adca7a6e83875f9302f7aed25
Sanket Dhengre <sanketdhengre@gmail.com>
2025-08-16 03:39:12 +0530

    Add frontend folder back to repository

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import Authentication from "./pages/authentication";
+import LandingPage from "./pages/landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VideoMeetComponent from "./pages/videoMeet.jsx";
+import HomeComponent from "./pages/home"; // Import HomeComponent
+import History from "./pages/history"; // Import History component
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <AuthProvider>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="/auth" element={<Authentication />} />
+                        <Route path="/home" s element={<HomeComponent />} />
+                        <Route path="/history" element={<History />} />
+                        <Route path="/:url" element={<VideoMeetComponent />} />
+                    </Routes>
+                </AuthProvider>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
