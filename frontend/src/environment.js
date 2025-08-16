92c0898b355c642dc43803f5351c13b324501def
Sanket Dhengre <sanketdhengre@gmail.com>
2025-08-16 20:04:43 +0530

    Deploying Frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,7 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+
+const server = IS_PROD
+    ? "https://nexcallbackend-a71c.onrender.com"
+    : "http://localhost:8000";
+
 export default server;
--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -17,8 +17,9 @@ import ChatIcon from "@mui/icons-material/Chat";
 import io from "socket.io-client";
 
 import styles from "../styles/videoComponent.module.css";
+import server from "../environment";
 
-const server_url = "http://localhost:8000";
+const server_url = server; // Use the production server URL
 
 var connections = {};
 
